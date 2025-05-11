5069171aba3c2e3a8d69164aa7e4c5cfb7a80f1d
SuperShrew <joshua.gerrish@outlook.com>
2025-05-11 20:30:55 +0100

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -5,21 +5,50 @@ import os
 
 arr=[]
 
-for i in range(0, 10):
-    arr.append(r.randint(1, 10))
+maxnum = int(input("maximum number? "))
+slep = input("slep? (enter if no) ")
+amount = int(input("list amount? "))
+for i in range(0, amount):
+    arr.append(r.randint(1, maxnum))
 
+print(len(arr))
 print(arr)
+print(arr[2])
+print(arr[6])
+print(arr[1])
 
 def insertion(array):
-    n = len(array)
-    for i in range(1, n):
-        x = i-1
-        while x >= 0 and i < array[x]:
-            array[x+1] = array[x]
-            x -= 1
-        array[x+1] = i
-        print(array)
-    print(array)
+    global maxnum
+    global slep
+    counter = 0
+    loadarray = ["loading.", "loading..", "loading..."]
+    a=0
+    if not(slep):
+        print(loadarray[a])
+    result = [maxnum+1]
+    for i in range(0, len(array)):
+        for x in range(0, len(result)):
+            counter += 1
+            #print("a")
+            #print(x)
+            if result[x] > array[i]:
+                result.insert(x, array[i])
+                break
+        if slep:
+            os.system("clear")
+            print("result:", result)
+            time.sleep(0.05)
+        else:
+            if counter > amount*100:
+                a+=1
+                if a > 2:
+                    a=0
+                os.system("clear")
+                print(loadarray[a])
+                counter = 0
+        #print("orig:", array)
+    result.pop(-1)
+    return result
 
-#DOESNT WORK DONT TRUST THIS IT REPLACES NUMBERS AAAAAAAAA
-insertion(arr)
+
+print(insertion(arr))
